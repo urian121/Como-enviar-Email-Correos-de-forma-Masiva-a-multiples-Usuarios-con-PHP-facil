d2be065e68181bdea62bf278aa0b57386f4fade8
URIAN VIERA <urian1213viera@gmail.com>
2021-11-12 10:32:31 -0500

    Update enviarEmail.php

--- enviarEmail.php
+++ enviarEmail.php
@@ -174,6 +174,60 @@ $cuerpo .= '
         $columna ++;
     }
  }
+    
+    
+/*
+    NOTA IMPORTANTE:
+    PARA ENVIAR CORREOS TANTO COMO  GMAIL - HOTMAIL(Outlook), ESTE CODIGO FUNCIONA MEJOR QUE EL ANTERIOR,
+    EN ESTE LLEGAN LOS EMAIL A Outlook EN BANDEJA DE ENTRADA AL IGUAL QUE GMAIL, SOLO DEBES CAMBIAR EL CONTENIDO 
+    DEL MESAJE QUE ENVIAS. OJO SI EXISTE ALGUNA URL O VARIABLE QUE ESTES ENVIANDO EN EL EMAIL Y NO EXISTE O ESTA 
+    INCORRECTA EL CORREO NO LLEGARA DEBES ESTA PENDIENTE EN ESTO.
+*/
+
+<?php
+/*
+    $para    = "programadorphp2017@gmail.com";
+    $nameFull = "Urian Viera";
+    $urlCanal = "https://www.youtube.com/channel/UCodSpPp_r_QnYIQYCjlyVGA";
+    $titulo  = "Mi Formulario de Contacto";
+    $mensaje = "
+    <!doctype html>
+    <html lang='es'>
+    <head>
+    <title>EMAIL</title>
+    </head>
+        <body>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <div style='width: 80%; margin:0 auto; background-color: #ffffff; color: #34495e; text-align: center;font-family: sans-serif'>
+                <h2 style='font-size: 16px; color: #34495e; margin: 0 0 7px;'>&#161;Felicitaciones&#33; 
+                    <strong style='color:#555;'> ".$nameFull."</strong>, dale click al bot&oacute;n para visualizar tu bono de descuento de <strong>$20.000</strong>. 
+                    Recuerda mostrarlo en la caja de cualquier sede del retailer escogido, no es necesario imprimirlo.
+                </h2>
+                <p>&nbsp;</p>
+                    <a href=".$urlCanal." style='background-color: #fe4c50;border: #fe4c50;color: white;text-decoration: none;padding: 10px 40px;border-radius: 5px;'> Ver cup&oacute;n </a>
+                <p>&nbsp;</p>
+                <img style='padding: 0; display: block; width:100%; max-width:600px; margin:0 auto;' src='https://blogangular-c7858.web.app/assets/images/work.gif'>
+                <p>&nbsp;</p>
+            </div>
+        </body>
+    </html>
+    "; 
+    //Cabecera Obligatoria
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: WebDeveloper <programadorphp2017@gmail.com>' . "\r\n";
+    $headers .= 'Cc: noresponder@pruebaroyalcanin.com.co' . "\r\n";
+    
+    //OPCIONAR
+    $headers .= "Reply-To: "; 
+    $headers .= "Return-path:"; 
+    $headers .= "Cc:"; 
+    $headers .= "Bcc:"; 
+    
+    mail($para, $titulo, $mensaje, $headers);
+  */
+
 }
 ?>
   

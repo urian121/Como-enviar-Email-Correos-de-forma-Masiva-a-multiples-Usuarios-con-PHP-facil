7b76cceb513cb3fa59c51ca9c8bedbd5480f5934
Urian Viera <urian1213viera@gmail.com>
2023-08-09 10:14:28 -0500

    actualizando url de imagenes

--- enviarEmail.php
+++ enviarEmail.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta content="initial-scale=1, shrink-to-fit=no, width=device-width" name="viewport">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -14,45 +15,48 @@
     <link rel="stylesheet" type="text/css" href="css/home.css">
     <title>Recibiendo Emails de clientes</title>
 </head>
+
 <body>
 
-<nav class="navbar navbar-light" style="background-color: #55468c !important;">
-  <a class="navbar-brand" href="#">
-   <strong style="color: #fff">WebDeveloper</strong>
-  </a>
-</nav>
+    <nav class="navbar navbar-light" style="background-color: #55468c !important;">
+        <a class="navbar-brand" href="#">
+            <strong style="color: #fff">WebDeveloper</strong>
+        </a>
+    </nav>
 
 
-<div class="container mt-5"> 
+    <div class="container mt-5">
 
-<?php
-header('Content-Type: text/html; charset=UTF-8'); 
-include('config.php');
-if (isset($_REQUEST['enviarform'])) {
-    if (is_array($_REQUEST['correo'])) {
-        $num_countries = count($_REQUEST['correo']);
-        $columna   = 1;
-?>
-    <div class="row text-center mb-5">
-        <div class="col-12">
-            <p>Ha enviado un correo a: <strong>( <?php echo $num_countries; ?> )</strong> Registros <hr></p>
-        </div>
-    </div>
+        <?php
+        header('Content-Type: text/html; charset=UTF-8');
+        include('config.php');
+        if (isset($_REQUEST['enviarform'])) {
+            if (is_array($_REQUEST['correo'])) {
+                $num_countries = count($_REQUEST['correo']);
+                $columna   = 1;
+        ?>
+                <div class="row text-center mb-5">
+                    <div class="col-12">
+                        <p>Ha enviado un correo a: <strong>( <?php echo $num_countries; ?> )</strong> Registros
+                            <hr>
+                        </p>
+                    </div>
+                </div>
 
 
-<?php
-foreach ($_REQUEST['correo'] as $key => $emailCliente) {
+        <?php
+                foreach ($_REQUEST['correo'] as $key => $emailCliente) {
 
-$cliente ='Urian Viera';
+                    $cliente = 'Urian Viera';
 
-$destinatario = trim($emailCliente); //Quitamos algun espacion en blanco
-$asunto       = "Bienvenido a WebDeveloper";
-$cuerpo = '
+                    $destinatario = trim($emailCliente); //Quitamos algun espacion en blanco
+                    $asunto       = "Bienvenido a WebDeveloper";
+                    $cuerpo = '
     <!DOCTYPE html>
     <html lang="es">
     <head>
     <title>Envio de email de forma masiva - Urian Viera</title>';
-$cuerpo .= ' 
+                    $cuerpo .= ' 
 <style>
     * {
         margin: 0;
@@ -109,26 +113,26 @@ $cuerpo .= '
     </style>
 ';
 
-$cuerpo .= '
+                    $cuerpo .= '
 </head>
 <body>
     <div class="contenedor">
-    <img class="imgBanner" src="https://catmanshopper.com/enviosmasivosdecorreos/imgs/banner2.png">
+    <img class="imgBanner" src="https://raw.githubusercontent.com/urian121/imagenes-proyectos-github/master/banner-correo-masivos-urian-viera.jpeg">
         <p>&nbsp;</p>
         <p>&nbsp;</p>
 
     <table style="max-width: 600px; padding: 10px; margin:0 auto; border-collapse: collapse;">
     <tr>
         <td style="padding: 0">
-            <img style="padding: 0; display: block" src="https://catmanshopper.com/enviosmasivosdecorreos/imgs/banner.jpg" width="100%">
+            <img style="padding: 0; display: block" src="https://raw.githubusercontent.com/urian121/imagenes-proyectos-github/master/urian-viera-perfil.png" width="100%">
         </td>
     </tr>
     
     <tr>
         <td style="background-color: #ffffff;">
             <div class="misection">
-                <h2 style="color: red; margin: 0 0 7px">Hola, '.$cliente.'</h2>
-                <p style="margin: 2px; font-size: 18px">te doy la Bienvenida a WebDeveloper, un canal de Desarrollo Web y '.utf8_decode("Programación").'. </p>
+                <h2 style="color: red; margin: 0 0 7px">Hola, ' . $cliente . '</h2>
+                <p style="margin: 2px; font-size: 18px">te doy la Bienvenida a WebDeveloper, a un canal de Desarrollo Web y Programaci&oacuten. </p>
             </div>
         </td>
     </tr>
@@ -136,7 +140,7 @@ $cuerpo .= '
         <td style="background-color: #ffffff;">
         <div class="misection">
             <h2 style="color: red; margin: 0 0 7px">Visitar Canal de Youtube</h2>
-            <img style="padding: 0; display: block" src="https://catmanshopper.com/enviosmasivosdecorreos/imgs/canal.png" width="100%">
+            <img style="padding: 0; display: block" src="https://raw.githubusercontent.com/urian121/imagenes-proyectos-github/master/videos-youtube-urian-viera.png" width="100%">
         </div>
         
         <div class="mb-5 misection">  
@@ -148,49 +152,50 @@ $cuerpo .= '
     </tr>
     <tr>
         <td style="padding: 0;">
-            <img style="padding: 0; display: block" src="https://catmanshopper.com/enviosmasivosdecorreos/imgs/footer.png" width="100%">
+            <img style="padding: 0; display: block" src="https://raw.githubusercontent.com/urian121/imagenes-proyectos-github/master/footer-correos-masivos-urian-viera.png" width="100%">
         </td>
     </tr>
-</table>'; 
+</table>';
 
-$cuerpo .= '
+                    $cuerpo .= '
       </div>
     </body>
   </html>';
-    
-    //Cabecera Obligatoria
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= 'From: WebDeveloper <programadorphp2017@gmail.com>' . "\r\n";
-    $headers .= 'Cc: noresponder@pruebaroyalcanin.com.co' . "\r\n";
-    
-    //OPCIONAR
-    $headers .= "Reply-To: "; 
-    $headers .= "Return-path:"; 
-    $headers .= "Cc:"; 
-    $headers .= "Bcc:"; 
-    
-    if(mail($destinatario,$asunto,$cuerpo,$headers)){
-        $UpdateEmail = ("UPDATE clientes SET notificacion='1'  WHERE correo='".$emailCliente."' ");
-        $resultEmail = mysqli_query($con, $UpdateEmail);
-    }
 
-        echo '<div>'. $columna. "). " .$emailCliente.'</div>';
-        $columna ++;
-    }
- }
-}
-?>
-  
-    <div class="row text-center mt-5 mb-5">
-        <div class="col-12">
-            <a href="index.php" class="btn btn-round btn-primary">Volver al Inicio</a>
+                    //Cabecera Obligatoria
+                    $headers = "MIME-Version: 1.0" . "\r\n";
+                    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+                    $headers .= 'From: WebDeveloper <programadorphp2017@gmail.com>' . "\r\n";
+                    $headers .= 'Cc: noresponder@pruebaroyalcanin.com.co' . "\r\n";
+
+                    //OPCIONAR
+                    $headers .= "Reply-To: ";
+                    $headers .= "Return-path:";
+                    $headers .= "Cc:";
+                    $headers .= "Bcc:";
+
+                    if (mail($destinatario, $asunto, $cuerpo, $headers)) {
+                        $UpdateEmail = ("UPDATE clientes SET notificacion='1'  WHERE correo='" . $emailCliente . "' ");
+                        $resultEmail = mysqli_query($con, $UpdateEmail);
+                    }
+
+                    echo '<div>' . $columna . "). " . $emailCliente . '</div>';
+                    $columna++;
+                }
+            }
+        }
+        ?>
+
+        <div class="row text-center mt-5 mb-5">
+            <div class="col-12">
+                <a href="index.php" class="btn btn-round btn-primary">Volver al Inicio</a>
+            </div>
         </div>
+
+
     </div>
 
 
-</div>
-
-
 </body>
+
 </html>
